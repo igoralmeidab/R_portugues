d17465780e0d175767f4ebfbf855b8cef8b39753
igoralmeidab <igor.almeidab@gmail.com>
2019-04-01 13:08:38 +0900

    Add files via upload

---
+++ media_mediana.R
@@ -0,0 +1,15 @@
+#medidas de tendência central
+
+dados<- c(1,3,3,5,2,7,4,9)
+
+
+
+#média aritmética
+#soma dos números dividido pela quantidade de números
+?mean
+mean(dados,na.rm=TRUE)
+
+#mediana
+#valor(es) do meio em uma série de números organizados de maneira crescente ou decrescente
+?median
+median(dados)
